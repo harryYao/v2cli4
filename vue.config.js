95ce1fea9a656e91af10e48cfbbe9b9b5d015f05
yaoxin <yaoxin1@ztgame.com>
2021-04-01 15:34:12 +0800

    add crm 环境

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 const webpack = require('webpack')
 const path = require('path');
 
+const IS_PROD = ["production", "prod"].includes(process.env.NODE_ENV);
+
 module.exports = {
-  
+  publicPath: IS_PROD ? process.env.VUE_APP_PUBLIC_PATH : "./", // 默认'/'，部署应用包时的基本 URL
+
 }
